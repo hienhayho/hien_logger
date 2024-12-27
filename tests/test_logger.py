8a4bf3a9652d59932d25541333e5e0e41832a003
Ho Trong Hien <115549171+hienhayho@users.noreply.github.com>
2024-12-28 00:30:29 +0700

    fix: add global_file_log to test.

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -24,3 +24,13 @@ def test_logger_with_folder():
 
     with open("logs/test.log", "r") as f:
         assert "Hello, World!" in f.read()
+
+
+def test_global_log_file():
+    logger = get_formatted_logger("hien_logger", global_file_log=True)
+
+    logger.info("Hello, World!")
+    assert Path("logs/global.log").exists()
+
+    with open("logs/global.log", "r") as f:
+        assert "Hello, World!" in f.read()
